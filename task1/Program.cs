94074282edfafefcbc59b8da99bb882c283554fb
Alyona <nakamura-san@mail.ru>
2022-10-19 14:19:19 +0300

    Task 1 and 2

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+
+int Print(int m, int n)
+{
+    if (n == m)
+        return n;
+    return n + Print(m, n-1);
+
+}
+
+Console.WriteLine("Введите первое значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма элементов от первого до второго значения равна: {Print(m, n)}");
---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+Console.Clear();
+
+int Print(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return Print(m-1, 1);
+    return Print(m-1,Print(m, n-1));
+
+}
+
+Console.WriteLine("Введите первое значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана для заданных значений равна: {Print(m, n)}");
